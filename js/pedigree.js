45acabcc5c8a3fe297905abd49b68aeac42ad966
tcarver <tjc29@cam.ac.uk>
2017-02-22 14:52:36 +0000

    centralise parent nodes if it doesn't lie between mother and father

--- js/pedigree.js
+++ js/pedigree.js
@@ -211,8 +211,8 @@
 					var father = pedigree_util.getNodeByName(flattenNodes, node.data.father.name);
 					var mother = pedigree_util.getNodeByName(flattenNodes, node.data.mother.name);
 					var xmid = (father.x + mother.x) /2;
-					node.x = xmid;   // centralise parent nodes
 					if(!pedigree_util.overlap(opts, root.descendants(), xmid, node.depth, [node.data.name])) {
+						node.x = xmid;   // centralise parent nodes
 						var diff = node.x - xmid;
 						if(node.children.length == 2 && (node.children[0].data.hidden || node.children[1].data.hidden)) {
 							if(!(node.children[0].data.hidden && node.children[1].data.hidden)) {
@@ -240,6 +240,8 @@
 								}
 							}
 						}
+					} else if((node.x < father.x && node.x < mother.x) || (node.x > father.x && node.x > mother.x)){
+							node.x = xmid;   // centralise parent nodes if it doesn't lie between mother and father
 					}
 				}
 			}
