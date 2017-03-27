db5726d1fdcdaef151dd2de9c1525fcd668750b6
tcarver <tjc29@cam.ac.uk>
2017-03-27 15:48:07 +0100

    include a display name

--- js/pedigree.js
+++ js/pedigree.js
@@ -342,9 +342,9 @@
 	ptree.build = function(options) {
         var opts = $.extend({ // defaults
         	targetDiv: 'pedigree_edit',
-        	dataset: [ {"name": "m21", "sex": "M", "top_level": true},
-        		       {"name": "f21", "sex": "F", "top_level": true},
-        			   {"name": "ch1", "sex": "F", "mother": "f21", "father": "m21", "proband": true}],
+        	dataset: [ {"name": "m21", "display_name": "father", "sex": "M", "top_level": true},
+        		       {"name": "f21", "display_name": "mother", "sex": "F", "top_level": true},
+        			   {"name": "ch1", "display_name": "me", "sex": "F", "mother": "f21", "father": "m21", "proband": true}],
         	width: 600, 
         	height: 400,
         	symbol_size: 35,
