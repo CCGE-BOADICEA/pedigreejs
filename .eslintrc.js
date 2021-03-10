b28f1df9a4111df7b05d342b0328b24711cbe959
tcarver <tjc29@medschl.cam.ac.uk>
2021-03-10 15:08:22 +0000

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "d3": true,
+        "canvg": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+	"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+	"no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
+};
