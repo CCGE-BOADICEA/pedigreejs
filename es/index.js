1ded34d4e0433d8062e7c0031585dfb4e797fb17
tcarver <tjc29@medschl.cam.ac.uk>
2021-03-11 16:03:32 +0000

    add canrisk_file

--- es/index.js
+++ es/index.js
@@ -5,3 +5,4 @@ export * as pedigreejs from './pedigree.js';
 export * as pedigree_utils from './pedigree_utils.js';
 export * as pedcache from './pedcache.js'; 
 export * as io from './io.js';
+export * as canrisk_file from './canrisk_file.js';
