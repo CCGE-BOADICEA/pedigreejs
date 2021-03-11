d929dff97d01865e2ff20e0f4fa1b6b5f7544c28
tcarver <tjc29@medschl.cam.ac.uk>
2021-03-11 13:39:18 +0000

    add css import

--- es/index.js
+++ es/index.js
@@ -1,4 +1,6 @@
 // aggregating modules
+import '../css/pedigreejs.css';
+
 export * as pedigreejs from './pedigree.js';
 export * as pedigree_utils from './pedigree_utils.js';
 export * as pedcache from './pedcache.js'; 
