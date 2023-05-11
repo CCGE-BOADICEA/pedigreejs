4e2fc1e5a6dcf2987200effd31f3802098087c7a
tcarver <tjc29@cam.ac.uk>
2023-05-11 17:54:41 +0100

    update get_printable_svg()

--- es/io.js
+++ es/io.js
@@ -175,16 +175,17 @@ function get_printable_svg(opts) {
 	let svg = $('#'+opts.targetDiv).find('svg').clone().appendTo(svg_div);
 
 	let a4 = {w: (595-40), h: (842-50)};
-	svg.attr('width', a4.w);
-	svg.attr('height', a4.h);
 	
 	let b = get_bounds(opts);
-	let d = {wid: Math.abs(b.xmax-b.xmin), hgt: Math.abs(b.ymax-b.ymin)};
-	let f = (a4.w-(opts.symbol_size*1.5))/a4.w;
-	let k = (f / Math.max(d.wid/a4.w, d.hgt/a4.h));
+	let d = {w: Math.abs(b.xmax-b.xmin), h: Math.abs(b.ymax-b.ymin)};
+	let f = 1;
+	let k = (f / Math.max(d.w/a4.w, d.h/a4.h));
 
-	let xi = -(b.xmin-(opts.symbol_size/2))*k;
-	let yi = -(b.ymin-(opts.symbol_size/2))*k;
+	let xi = -(b.xmin)*k;
+	let yi = -(b.ymin-(opts.symbol_size))*k;
+
+	svg.attr('width', a4.w);
+	svg.attr('height', (d.h+opts.symbol_size+opts.symbol_size)*k);	
 
 	svg.find(".diagram").attr("transform", "translate("+xi+", "+yi+") scale("+k+")");
 	return svg_div;
